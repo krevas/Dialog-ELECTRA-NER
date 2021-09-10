86e8da8b55be5b21484bd09ad0bde74cded2fb34
krevas <krevas@naver.com>
2021-09-11 00:30:30 +0900

    fix crawler

--- naver_news.py
+++ naver_news.py
@@ -21,26 +21,36 @@ def get_naver_news():
         pages = [num for num in range(1,5)]
         page = random.choice(pages)
 
-        sec_url = "https://news.naver.com/main/list.nhn?mode=LPOD&sid2=140&sid1=001&mid=sec&oid=001&isYeonhapFlash=Y&date={}&page={}".format(date,page)
+        sec_url = "https://news.naver.com/main/list.naver?mode=LPOD&mid=sec&oid=023&date={}&page={}".format(date,page)
         soup = get_soup_obj(sec_url)
-
-        lis = soup.find('ul', class_='type02').find_all("li", limit=9)
+        
+        
+        lis = soup.find('ul', class_='type06').find_all("li", limit=9)
         li = random.choice(lis)
         news_url = li.a.attrs.get('href')
+        
         soup = get_soup_obj(news_url)
         try:
             text = soup.find('div', id='articleBodyContents').get_text()
             text = text.replace('// flash 오류를 우회하기 위한 함수 추가','')
             text = text.replace('function _flash_removeCallback() {}','')
             text = text.strip()
+            """
             try:
                 for match in re.finditer(r'\(\S*=연합뉴스\)',text):
                     pass
                 start = match.start()
             except:
                 start = 0
-            if len(text[start::]) > 500 and len(text[start::]) < 5000:
+            """
+            #if len(text[start::]) > 500 and len(text[start::]) < 5000:
+            if len(text) > 500 and len(text) < 2000:
                 break
         except:
             pass
-    return text[start::]
+    
+    return text
+
+if __name__ == "__main__":
+	text=get_naver_news()
+	print(text)
